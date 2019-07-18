ecc710fd1c5f45e4ba59605a7ac1ae93875d83c7
jeff garrett <jeffgarrett@jeffs-MacBook-Pro.local>
2019-07-17 22:36:30 -0700

    check-in

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "rds_engine" {
+  description = "engine type"
+  default = "aurora"
+}
+
+variable "rds_vpc_security_group_ids" {
+  default = null
+}
+
+variable "rds_database_name" {
+  description = "name of the database"
+  default = "PATS_Test"
+}
+
+variable "rds_backup_retention_period" {
+  description = "number of days to hold backups"
+  default = 1
+}
+
+variable "rds_preferred_backup_window" {
+  description = "description"
+  default = "07:00-09:00"
+}
+
+variable "rds_deletion_protection" {
+  description = "description"
+  default = false
+}
+
+variable "rds_port" {
+  description = "Port database connects to"
+  default = "5432"
+}
